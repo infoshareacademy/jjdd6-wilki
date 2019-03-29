13f667886091364d5768adbe217148c780d43190
Piotr Wenta <buraxta@gmail.com>
2019-03-29 16:17:38 +0100

    JJDD6WIL-11 Class modelling: first attempt

---
+++ src/main/java/com/infoshareacademy/Main.java
@@ -0,0 +1,9 @@
+package com.infoshareacademy;
+
+public class Main {
+    public static void main(String[] args) {
+        AppRunner appRunner = new AppRunner();
+        appRunner.run();
+
+    }
+}
---
+++ src/main/java/com/infoshareacademy/User.java
@@ -0,0 +1,4 @@
+package com.infoshareacademy;
+
+public class User {
+}
