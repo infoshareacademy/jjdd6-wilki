2ece2703ce0f7b40fe50ecd9d451f9060fb01428
Piotr Wenta <buraxta@gmail.com>
2019-03-29 16:43:07 +0100

    JJDD6WIL-11 Class modelling: fields and comments added

--- src/main/java/com/infoshareacademy/Share.java
+++ src/main/java/com/infoshareacademy/Share.java
@@ -1,4 +1,29 @@
 package com.infoshareacademy;
 
+import java.math.BigDecimal;
+
 public class Share {
+    private String ticker;
+    private String fullCompanyName;
+    private BigDecimal currentPrice;
+    private BigDecimal takeProfitPrice;
+    private BigDecimal stopLossPrice;
+    private Integer sharesAmount;
+    private BigDecimal avgBuyPrice;
+    private Double currentPE;
+    private Double targetPE;
+    private Double riskRewardRatio;
+    private Long volume;
+
+
+    // targetPE = takeProfitPrice / (currentPE / currentPrice)
+    // currentPrice = cena zamkniecia z pliku CSV
+    // riskRewardRatio = (avgBuyPrice - stopLossPrice) / (takeProfitPrice - stopLossPrice)
+    // Lista z aktualnym stanem do wyliczenia avgBuyPrice i sharesAmount. Przy kupnie do listy dodany bedzie nowy wiersz, a sprzedaz bedzie zgodnie z metoda FIFO
+    // Lista z historia transakcji do podgladu dla uzytkownika
+
+    
+
+
+
 }
