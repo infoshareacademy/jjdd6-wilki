65720f11c1351c19745de73d4b80d4511fd441e8
Piotr Wenta <buraxta@gmail.com>
2019-04-02 17:06:03 +0200

    JJDD6WIL-2: constructors added

--- src/main/java/com/infoshareacademy/jjdd6/wilki/Share.java
+++ src/main/java/com/infoshareacademy/jjdd6/wilki/Share.java
@@ -2,6 +2,8 @@ package com.infoshareacademy.jjdd6.wilki;
 
 import java.math.BigDecimal;
 
+import static javax.print.attribute.standard.MediaSizeName.B;
+
 public class Share {
     private String ticker;
     private String fullCompanyName;
@@ -10,6 +12,7 @@ public class Share {
     private BigDecimal stopLossPrice;
     private Integer sharesAmount;
     private BigDecimal avgBuyPrice;
+    private BigDecimal buyPrice;
     private Double currentPE;
     private Double targetPE;
     private Double riskRewardRatio;
@@ -23,20 +26,25 @@ public class Share {
     // Lista z historia transakcji do podgladu dla uzytkownika
 
     public void run() {
+        Share share = new Share("KGH", 100,BigDecimal.valueOf(103.75));
+        
 
 
     }
 
-    public Share(String ticker, BigDecimal takeProfitPrice, BigDecimal stopLossPrice, Integer sharesAmount) {
+    public Share(String ticker, Integer sharesAmount, BigDecimal buyPrice, BigDecimal takeProfitPrice, BigDecimal stopLossPrice) {
         this.ticker = ticker;
         this.takeProfitPrice = takeProfitPrice;
         this.stopLossPrice = stopLossPrice;
         this.sharesAmount = sharesAmount;
+        this.buyPrice = buyPrice;
     }
 
-
-    public Share(String ticker, Integer sharesAmount) {
+    public Share(String ticker, Integer sharesAmount, BigDecimal buyPrice) {
         this.ticker = ticker;
+        this.buyPrice = buyPrice;
         this.sharesAmount = sharesAmount;
     }
+
+
 }
