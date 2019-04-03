aa678ec03cf3777f1517cc15f257063707e40ee9
Maciej <mysz34@poczta.onet.pl>
2019-04-03 17:09:00 +0200

    First commit on Text Menu branch

--- src/main/java/com/infoshareacademy/jjdd6/wilki/AppRunner.java
+++ src/main/java/com/infoshareacademy/jjdd6/wilki/AppRunner.java
@@ -1,7 +1,54 @@
 package com.infoshareacademy.jjdd6.wilki;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class AppRunner {
     public void run() {
+
+        TextInterface textInterface = new TextInterface();
+        textInterface.drawMenu();
+
+        chooseOption();
+
+    }
+    public void chooseOption() throws InputMismatchException {
+        Scanner input = new Scanner(System.in);
+        try {
+            while(flag) {
+            int choice = input.nextInt();
+            switch (choice) {
+                case 1: {
+                    System.out.println("1");
+                    break;
+                }
+                case 2: {
+                    System.out.println("2");
+                    break;
+                }
+                case 3: {
+                    System.out.println("3");
+                    break;
+                }
+                case 4: {
+                    System.out.println("4");
+                    break;
+                }
+                case 5: {
+                    System.out.println("5");
+                    break;
+                }
+                default: {
+                    System.out.println("Try again");
+//                    chooseOption();
+                }
+            }
+            }
+        }
+        catch (InputMismatchException e){
+            System.out.println("Input valid number");
+            chooseOption();
+        }
 
     }
 }
