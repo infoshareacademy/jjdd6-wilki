171754cf636b3806afddb824c8f76292ec6ad6ed
Arkadiusz Kucmann <arkadiusz.kucmann@gmail.com>
2019-04-03 20:27:23 +0200

    Removed unnecessary throws IOException from Main

--- src/main/java/com/infoshareacademy/jjdd6/wilki/Main.java
+++ src/main/java/com/infoshareacademy/jjdd6/wilki/Main.java
@@ -1,9 +1,7 @@
 package com.infoshareacademy.jjdd6.wilki;
 
-import java.io.IOException;
-
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         AppRunner appRunner = new AppRunner();
         appRunner.run();
 
