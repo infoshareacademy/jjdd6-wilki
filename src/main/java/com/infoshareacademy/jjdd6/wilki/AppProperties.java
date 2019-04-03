f10fc1175f2cb627a9bf997b0d7585c2ad3ca18b
Arkadiusz Kucmann <arkadiusz.kucmann@gmail.com>
2019-04-03 20:21:11 +0200

    Implemented correction to AppProperties class

--- src/main/java/com/infoshareacademy/jjdd6/wilki/AppProperties.java
+++ src/main/java/com/infoshareacademy/jjdd6/wilki/AppProperties.java
@@ -3,46 +3,49 @@ package com.infoshareacademy.jjdd6.wilki;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.text.SimpleDateFormat;
 import java.util.Properties;
-
-import static javax.swing.plaf.basic.BasicHTML.propertyKey;
 
 public class AppProperties {
 
-    public static final String DATE_FORMAT = "dateFormat";
-    public static final String CONFIG_PROPERTIES_FILENAME = "config.properties";
+    private static final String DATE_FORMAT = "dateFormat";
+    private static final String CONFIG_PROPERTIES_FILENAME = "config.properties";
 
-    private Properties testProperties = null;
+    public String getProperties() {
 
-    public void getProperties(String property) throws IOException {
-    if (testProperties == null) {
-            InputStream inputStream = new FileInputStream(CONFIG_PROPERTIES_FILENAME);
-            testProperties = new Properties();
-            testProperties.load(inputStream);
-            inputStream.close();
+        Properties properties = new Properties();
+        InputStream input = null;
+
+        String dateFormat = null;
+        try {
+            input = new FileInputStream(CONFIG_PROPERTIES_FILENAME);
+
+            properties.load(input);
+
+            dateFormat = properties.getProperty(DATE_FORMAT);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (input != null) {
+                try {
+                    input.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
-        testProperties.getProperty(propertyKey);
+        }
+        return dateFormat;
+    }
 
-    }}
-
-//    private String getPropertyValue(String propertyKey) {
-//        java.util.Properties properties = new java.util.Properties();
-//
-//        //try-with-resources
-//        try (InputStream input = new FileInputStream(CONFIG_PROPERTIES_FILENAME)) {
-//
-//            properties.load(input);
-//            properties.values();
-//            validate(values);
-//
-//            String value = properties.getProperty(propertyKey);
-//
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        } catch (IllegalArgumentException e) {
-//            System.out.println("Error while loading \"Illegal pattern character 'x'\"");
-//        }
-//        return null;
-//    }
-
-
+    public void checkDate() {
+        AppProperties appProperties = new AppProperties();
+        try {
+            new SimpleDateFormat(appProperties.getProperties());
+        } catch (IllegalArgumentException e) {
+            System.out.println("Error while loading " +
+                    "\"Illegal pattern character 'x'\" " +
+                    "(dateFormat in config.properties)");
+        }
+    }
+}
--- src/main/java/com/infoshareacademy/jjdd6/wilki/AppRunner.java
+++ src/main/java/com/infoshareacademy/jjdd6/wilki/AppRunner.java
@@ -1,26 +1,10 @@
 package com.infoshareacademy.jjdd6.wilki;
 
-import java.io.IOException;
-
 public class AppRunner {
-    public void run() throws IOException {
+    public void run() {
 
         AppProperties appProperties = new AppProperties();
-        appProperties.getProperties(AppProperties.DATE_FORMAT);
+        appProperties.checkDate();
+
     }
-
-//        String appName = AppProperties.getPropertyValue(AppProperties.APPLICATION_NAME);
-
-//        String dateFormat = AppProperties.getPropertyValue(AppProperties.DATE_FORMAT);
-//
-//        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(Objects.requireNonNull(dateFormat));
-////
-//        Date localDate = new Date();
-////
-////        String localDate = simpleDateFormat.format(LocalDate.now());
-////
-//        String date = simpleDateFormat.format(localDate);
-////
-//        System.out.println(date);
-//    }
 }
