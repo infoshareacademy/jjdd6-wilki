14248045686a16ee270ca40a7aa2fc7be4914ba1
Arkadiusz Kucmann <arkadiusz.kucmann@gmail.com>
2019-04-03 16:08:09 +0200

    config.properties - ready

--- src/main/java/com/infoshareacademy/jjdd6/wilki/AppRunner.java
+++ src/main/java/com/infoshareacademy/jjdd6/wilki/AppRunner.java
@@ -1,12 +1,23 @@
 package com.infoshareacademy.jjdd6.wilki;
 
-import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Objects;
 
 public class AppRunner {
-    public void run() throws IOException {
+    public void run() {
 
-        Properties properties = new Properties();
-        properties.getPropertiesValues();
+//        String appName = Properties.getPropertyValue(Properties.APPLICATION_NAME);
 
+        String dateFormat = Properties.getPropertyValue(Properties.DATE_FORMAT);
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(Objects.requireNonNull(dateFormat));
+
+        Date localDate = new Date();
+
+//        String localDate = simpleDateFormat.format(LocalDate.now());
+
+        String date = simpleDateFormat.format(localDate);
+
+        System.out.println(date);
     }
 }
--- src/main/java/com/infoshareacademy/jjdd6/wilki/Properties.java
+++ src/main/java/com/infoshareacademy/jjdd6/wilki/Properties.java
@@ -3,38 +3,25 @@ package com.infoshareacademy.jjdd6.wilki;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Date;
 
 public class Properties {
 
-    public String getPropertiesValues() throws IOException {
+    public static final String APPLICATION_NAME = "ApplicationName";
+    public static final String DATE_FORMAT = "dateFormat";
+    public static final String CONFIG_PROPERTIES_FILENAME = "config.properties";
 
+    public static String getPropertyValue(String propertyKey) {
         java.util.Properties properties = new java.util.Properties();
-        InputStream input = null;
 
-        try {
-            input = new FileInputStream("config.properties");
+        //try-with-resources
+        try (InputStream input = new FileInputStream(CONFIG_PROPERTIES_FILENAME)) {
 
             properties.load(input);
 
-            Date time = new Date(System.currentTimeMillis());
-
-            // get the property value and print it out
-            String name = properties.getProperty("ApplicationName");
-            String output = "\nWelcome to " + name;
-
-            System.out.println(output + "\nApplication ran on " + time);
+            return properties.getProperty(propertyKey);
 
         } catch (IOException e) {
             e.printStackTrace();
-        } finally {
-            if (input != null) {
-                try {
-                    input.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
         }
         return null;
     }
