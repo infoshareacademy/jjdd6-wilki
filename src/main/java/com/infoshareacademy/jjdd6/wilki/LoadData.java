cf723fc3462a11e59fa3dcb5611a86b348f2431e
Ewelina Turska <ewelinaturska27@gmail.com>
2019-04-04 17:01:49 +0200

    DataFromFile class added

--- src/main/java/com/infoshareacademy/jjdd6/wilki/LoadData.java
+++ src/main/java/com/infoshareacademy/jjdd6/wilki/LoadData.java
@@ -4,15 +4,19 @@ import com.opencsv.CSVReader;
 
 import java.io.FileReader;
 import java.io.IOException;
+import java.math.BigDecimal;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class LoadData {
-    public List read(String file) throws IOException {
-//        String filename = Properties.getValue(wig_20);
-        String userDir = System.getProperty("user.dir");
-//        Path path = Paths.get(userDir,"Downloads",filename);
-//       List<String> lines = Files.readAllLines(path);
+    public List read(String file) {
+
         List<String[]> allData = new ArrayList<>();
         try {
             FileReader fileReader = new FileReader(file);
@@ -24,14 +28,36 @@ public class LoadData {
         return allData;
     }
 
-    public static void main(String[] args) {
-        try {
-            LoadData loadData = new LoadData();
-            List dataLoaded = loadData.read("/home/ewelina/Downloads/wig20.csv");
-
-        } catch (IOException e) {
-            e.printStackTrace();
+    public List<DataFromFile> loadToList(String[] args) {
+        if (args.length != 1) {
+            System.out.println("Prosze podac sciezke do danych");
+            System.exit(1);
         }
+
+        String path = args[0];
+        System.out.println("Podana sciezka to: " + path);
+
+//        try {
+            LoadData loadData = new LoadData();
+            List<String[]> dataLoaded = loadData.read(path);
+            List<DataFromFile> collect = dataLoaded.stream()
+                    .skip(1)
+                    .map(a -> {
+                        DataFromFile dataFromFile = new DataFromFile();
+                        dataFromFile.setSymbol(a[0]);
+                        dataFromFile.setDate(LocalDate.parse(a[1]));
+                        dataFromFile.setTime(LocalTime.parse(a[2]));
+                        dataFromFile.setOpeningPrice(new BigDecimal(a[3]));
+                        dataFromFile.setHighestPrice(new BigDecimal(a[4]));
+                        dataFromFile.setLowestPrice(new BigDecimal(a[5]));
+                        dataFromFile.setClosingPrice(new BigDecimal(a[6]));
+                        dataFromFile.setVolume(Long.parseLong(a[7]));
+                        return dataFromFile;
+                    }).collect(Collectors.toList());
+            return collect;
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
 
     }
 }
