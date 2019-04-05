0b4ae021f5a0c92f624433c4ae97d2c4ce9247b7
Piotr Wenta <buraxta@gmail.com>
2019-04-05 16:11:37 +0200

    JJDD6WIL-2: merge other branches

--- src/main/java/com/infoshareacademy/jjdd6/wilki/AppProperties.java
+++ src/main/java/com/infoshareacademy/jjdd6/wilki/AppProperties.java
@@ -3,8 +3,12 @@ package com.infoshareacademy.jjdd6.wilki;
 import java.io.FileInputStream;
 import java.time.format.DateTimeFormatter;
 import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class AppProperties {
+
+    private final static Logger logger = Logger.getLogger(Logger.getLogger("").getClass().getName());
 
     private static final String DATE_FORMAT = "dateFormat";
     private static final String DEFAULT_DATE_FORMAT = "yyyy-MM-dd";
@@ -20,10 +24,13 @@ public class AppProperties {
             properties.load(new FileInputStream(CONFIG_PROPERTIES_FILENAME));
             String dateFormat = properties.getProperty(DATE_FORMAT);
             dateTimeFormatter = DateTimeFormatter.ofPattern(dateFormat);
+            logger.log(Level.INFO, "Configuration file loading properly.");
         } catch (Exception e) {
             System.err.println("Error while loading configuration: " + e.getMessage());
-            System.err.println("Using default value: " + DEFAULT_DATE_FORMAT);
+            logger.log(Level.WARNING, "Error while loading configuration: " + e.getMessage());
             dateTimeFormatter = DateTimeFormatter.ofPattern(DEFAULT_DATE_FORMAT);
+            System.err.println("Using default value: " + DEFAULT_DATE_FORMAT);
+            logger.log(Level.INFO, "Default DateTimeFormatter created.");
         }
     }
 
