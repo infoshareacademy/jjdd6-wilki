f092943431633fe72874b79fc01e9801977db6ee
Piotr Wenta <buraxta@gmail.com>
2019-04-05 16:15:17 +0200

    Merge branch 'JJDD6WIL-2_Add_share' of /home/pewu/IdeaProjects/jjdd6-wilki with conflicts.

--- src/main/java/com/infoshareacademy/jjdd6/wilki/AppRunner.java
+++ src/main/java/com/infoshareacademy/jjdd6/wilki/AppRunner.java
@@ -1,47 +1,25 @@
 package com.infoshareacademy.jjdd6.wilki;
 
-import java.util.InputMismatchException;
-import java.util.Scanner;
+import java.time.LocalDate;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class AppRunner {
+
+    private final static Logger logger = Logger.getLogger(Logger.getLogger("").getClass().getName());
+
     public void run() {
 
-        TextInterface textInterface = new TextInterface();
-        textInterface.drawMenu();
+        Loggers.init();
+        logger.log(Level.INFO, "LoggerFile initialized properly.");
 
-        chooseOption();
+        String s = AppProperties.getDateFormat().format(LocalDate.now());
+        System.out.println(s);
 
-    }
-    private void chooseOption() throws InputMismatchException {
-        int choose = 0;
-        do{
-            Scanner input = new Scanner(System.in);
-            try{
-                choose = input.nextInt();
-                if(choose == 1){
-                    // show wallet
-                }
-                else if(choose == 2){
-                    // add/
-                }
-                else if(choose == 3){
-
-                }
-                else if(choose == 4){
-
-                }
-                else if(choose == 5){
-
-                }
-                else{
-                    System.out.println("Try again");
-                }
-
-            }catch (InputMismatchException e){
-                System.out.println("Input valid number");
-            }
-        }while(choose < 1 || choose > 5);
-
+        LocalDate date1 = LocalDate.of(2019, 5, 15);
+        String date = date1.format(AppProperties.getDateFormat());
+        System.out.println(date);
+        logger.log(Level.INFO, "Property formatted date printed.");
 
     }
 }
