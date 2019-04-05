3481d161f132093d5aef877aaf2ee25416249980
Arkadiusz Kucmann <arkadiusz.kucmann@gmail.com>
2019-04-05 15:54:01 +0200

    small change in Loggers class

--- src/main/java/com/infoshareacademy/jjdd6/wilki/Loggers.java
+++ src/main/java/com/infoshareacademy/jjdd6/wilki/Loggers.java
@@ -2,6 +2,7 @@ package com.infoshareacademy.jjdd6.wilki;
 
 import java.io.IOException;
 import java.util.logging.FileHandler;
+import java.util.logging.Logger;
 import java.util.logging.SimpleFormatter;
 
 public class Loggers {
@@ -14,7 +15,7 @@ public class Loggers {
         } catch (SecurityException | IOException e) {
             e.printStackTrace();
         }
-        java.util.logging.Logger logger = java.util.logging.Logger.getLogger("");
+        Logger logger = Logger.getLogger("");
         fileHandler.setFormatter(new SimpleFormatter());
         logger.addHandler(fileHandler);
     }
