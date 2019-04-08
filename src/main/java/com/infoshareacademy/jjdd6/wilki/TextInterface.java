1bea3d1a91bbbca63dd8b0016fd5b62dc14f3958
Maciej <mysz34@poczta.onet.pl>
2019-04-08 16:06:51 +0200

    Text interface class formatted

--- src/main/java/com/infoshareacademy/jjdd6/wilki/TextInterface.java
+++ src/main/java/com/infoshareacademy/jjdd6/wilki/TextInterface.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 
 public class TextInterface {
 
-    public void drawMenu(){
+    public void drawMenu() {
         String menu = "------------------------------------------------------------------" +
                 "\n| [1] Show wallet \t \t \t \t \t \t \t \t \t \t \t \t |" +
                 "\n| [2] Add new stocks / Increase stocks amount \t \t \t \t \t |" +
@@ -18,42 +18,37 @@ public class TextInterface {
 
     public void chooseOption() throws InputMismatchException {
         int choose = 0;
-        do{
+        do {
             Scanner input = new Scanner(System.in);
-            try{
+            try {
                 System.out.print("Choose option: ");
                 choose = input.nextInt();
-                if(choose == 1){
+                if (choose == 1) {
                     clearScreen();
 //                  Show wallet
-                }
-                else if(choose == 2){
+                } else if (choose == 2) {
                     clearScreen();
 //                  Add new stocks / Increase stocks amount
-                }
-                else if(choose == 3){
+                } else if (choose == 3) {
                     clearScreen();
 //                  Delete stocks / Reduce stocks amount
-                }
-                else if(choose == 4){
+                } else if (choose == 4) {
                     clearScreen();
 //                  Change parameters of your stocks (e.g. stop-loss price)
-                }
-                else if(choose == 5){
+                } else if (choose == 5) {
                     clearScreen();
 //                  Add / Remove money amount
-                }
-                else{
+                } else {
                     clearScreen();
                     drawMenu();
                     System.out.println("Try again");
                 }
-            }catch (InputMismatchException e){
+            } catch (InputMismatchException e) {
                 clearScreen();
                 drawMenu();
                 System.out.println("Input valid number");
             }
-        }while(choose < 1 || choose > 5);
+        } while (choose < 1 || choose > 5);
     }
 
     public void clearScreen() {
