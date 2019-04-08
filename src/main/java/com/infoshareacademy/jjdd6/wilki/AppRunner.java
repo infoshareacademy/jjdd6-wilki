3c0b0ec5f6588fde8bf28f1b53ebf7a2cd0f8961
Piotr Wenta <buraxta@gmail.com>
2019-04-08 10:31:27 +0200

    JJDD6WIL-2_Add_share: minor updates

--- src/main/java/com/infoshareacademy/jjdd6/wilki/AppRunner.java
+++ src/main/java/com/infoshareacademy/jjdd6/wilki/AppRunner.java
@@ -1,20 +1,25 @@
 package com.infoshareacademy.jjdd6.wilki;
 
-import java.io.File;
-import java.nio.file.Path;
+import java.time.LocalDate;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class AppRunner {
-    public void run(String[] args) {
-        if (args.length != 1) {
-            System.out.println("Prosze podac sciezke do danych");
-            System.exit(1);
-        }
 
-        String path = args[0];
-        System.out.println("Podana sciezka to: " + path);
+    private final static Logger logger = Logger.getLogger(Logger.getLogger("").getClass().getName());
 
-        LoadData loadData = new LoadData();
-        loadData.listFilesForFolder();
+    public void run() {
+
+        Loggers.init();
+        logger.log(Level.INFO, "LoggerFile initialized properly.");
+
+        String s = AppProperties.getDateFormat().format(LocalDate.now());
+        System.out.println(s);
+
+        LocalDate date1 = LocalDate.of(2019, 5, 15);
+        String date = date1.format(AppProperties.getDateFormat());
+        System.out.println(date);
+        logger.log(Level.INFO, "Property formatted date printed.");
 
     }
 }
--- src/main/java/com/infoshareacademy/jjdd6/wilki/Transaction.java
+++ src/main/java/com/infoshareacademy/jjdd6/wilki/Transaction.java
@@ -1,18 +1,22 @@
 package com.infoshareacademy.jjdd6.wilki;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 
 public class Transaction {
     private Integer amount;
     private BigDecimal price;
     private BigDecimal profit;
+    private LocalDate date;
 
     public Transaction(Integer amount, BigDecimal price) {
+        this.date = LocalDate.now();
         this.amount = amount;
         this.price = price;
     }
 
     public Transaction(Integer amount, BigDecimal price, BigDecimal profit) {
+        this.date = LocalDate.now();
         this.amount = amount;
         this.price = price;
         this.profit = profit;
