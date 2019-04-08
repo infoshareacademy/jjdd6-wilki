20c9990c073b40f0a0c8a24e44293dbf12974c38
Piotr Wenta <buraxta@gmail.com>
2019-04-08 15:44:02 +0200

    JJDD6WIL-2 + JJDD6WIL-3 + JJDD6WIL-5

---
+++ src/main/java/com/infoshareacademy/jjdd6/wilki/Main.java
@@ -0,0 +1,9 @@
+package com.infoshareacademy.jjdd6.wilki;
+
+public class Main {
+    public static void main(String[] args) {
+        AppRunner appRunner = new AppRunner();
+        appRunner.run();
+
+    }
+}
