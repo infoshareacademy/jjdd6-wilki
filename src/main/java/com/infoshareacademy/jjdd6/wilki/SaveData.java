7245013c19d585ae94c4b37ccfdf70c986b66583
Ewelina Turska <ewelinaturska27@gmail.com>
2019-04-09 16:07:51 +0200

    SaveData class updated

--- src/main/java/com/infoshareacademy/jjdd6/wilki/SaveData.java
+++ src/main/java/com/infoshareacademy/jjdd6/wilki/SaveData.java
@@ -1,4 +1,29 @@
 package com.infoshareacademy.jjdd6.wilki;
 
+import java.beans.XMLEncoder;
+import java.io.BufferedOutputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
 public class SaveData {
+
+
+    private static final String SERIALIZED_FILE_NAME = "wallet.xml";
+
+    public void serializeToXml() {
+        Wallet wallet = new Wallet();
+        XMLEncoder encoder = null;
+        try {
+            encoder = new XMLEncoder(new BufferedOutputStream(new FileOutputStream(SERIALIZED_FILE_NAME)));
+        } catch (FileNotFoundException fileNotFound) {
+            System.out.println("ERROR: While creating or opening file wallet.xml");
+        }
+        if (encoder != null) {
+            encoder.writeObject(wallet);
+        }
+        else System.out.println("Nothing to save!");
+        if (encoder != null) {
+            encoder.close();
+        }
+    }
 }
