039ba0023eec29756bd08845bbacfeee4ca8aa92
Piotr Wenta <buraxta@gmail.com>
2019-04-09 21:49:28 +0200

    Merge branches 'PW' and 'develop' of https://github.com/infoshareacademy/jjdd6-wilki into PW
    # Conflicts:
    #	src/main/java/com/infoshareacademy/jjdd6/wilki/AppRunner.java

--- src/main/java/com/infoshareacademy/jjdd6/wilki/AppRunner.java
+++ src/main/java/com/infoshareacademy/jjdd6/wilki/AppRunner.java
@@ -12,27 +12,24 @@ public class AppRunner {
 
     public void run() {
 
-        WalletTest test = new WalletTest();
-        test.walletTest();
+        LoadData loadData = new LoadData();
+        File folder = new File("./data");
+        loadData.listFilesForFolder(folder);
 
-//        LoadData loadData = new LoadData();
-//        File folder = new File("./data");
-//        loadData.listFilesForFolder(folder);
-//
-//        System.out.println("---");
-//
-//        String s = AppProperties.getDateFormat().format(LocalDate.now());
-//        System.out.println(s);
-//        LocalDate date1 = LocalDate.of(2019, 5, 15);
-//        String date = date1.format(AppProperties.getDateFormat());
-//        System.out.println(date);
-//        logger.info("Property formatted date printed.");
-//
-//        System.out.println("---");
-//
-//        TextInterface textInterface = new TextInterface();
-//        textInterface.drawMenu();
-//        textInterface.chooseOption();
+        System.out.println("---");
+
+        String s = AppProperties.getDateFormat().format(LocalDate.now());
+        System.out.println(s);
+        LocalDate date1 = LocalDate.of(2019, 5, 15);
+        String date = date1.format(AppProperties.getDateFormat());
+        System.out.println(date);
+        logger.info("Property formatted date printed.");
+
+        System.out.println("---");
+
+        TextInterface textInterface = new TextInterface();
+        textInterface.drawMenu();
+        textInterface.chooseOption();
 
     }
 }
