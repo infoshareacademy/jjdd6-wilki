b41003a4d0fab05c434bf6983cbb7d31c6728295
Piotr Wenta <buraxta@gmail.com>
2019-05-08 09:16:21 +0200

    added contenttype filter

---
+++ web/src/main/java/com/infoshareacademy/jjdd6/filter/SetContentTypeFilter.java
@@ -0,0 +1,28 @@
+package com.infoshareacademy.jjdd6.filter;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebFilter(
+        filterName = "SetContentTypeFilter",
+        urlPatterns = {"/*"})
+public class SetContentTypeFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletResponse resp = (HttpServletResponse) servletResponse;
+        resp.setContentType("text/html;charset=UTF-8");
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+}
