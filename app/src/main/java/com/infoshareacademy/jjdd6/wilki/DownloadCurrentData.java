8e586ab8802dc8779f0bba69cf67fe3b25b26b11
Piotr Wenta <buraxta@gmail.com>
2019-05-15 22:29:22 +0200

    minor changes

--- app/src/main/java/com/infoshareacademy/jjdd6/wilki/DownloadCurrentData.java
+++ app/src/main/java/com/infoshareacademy/jjdd6/wilki/DownloadCurrentData.java
@@ -5,7 +5,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.enterprise.context.RequestScoped;
-import javax.transaction.Transactional;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
