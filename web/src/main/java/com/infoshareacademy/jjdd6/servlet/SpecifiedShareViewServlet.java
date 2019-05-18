be4d8c00a43a386bab6b05df37edbe2897d0c12a
Maciej Myszkowski <mysz34@poczta.onet.pl>
2019-05-18 18:31:34 +0200

    show specified share in progress

--- web/src/main/java/com/infoshareacademy/jjdd6/servlet/SpecifiedShareViewServlet.java
+++ web/src/main/java/com/infoshareacademy/jjdd6/servlet/SpecifiedShareViewServlet.java
@@ -4,23 +4,24 @@ import com.infoshareacademy.jjdd6.freemarker.TemplateProvider;
 import com.infoshareacademy.jjdd6.service.StatsService;
 import com.infoshareacademy.jjdd6.service.UserService;
 import com.infoshareacademy.jjdd6.validation.Validators;
-import com.infoshareacademy.jjdd6.wilki.DownloadCurrentData;
-import com.infoshareacademy.jjdd6.wilki.Share;
-import com.infoshareacademy.jjdd6.wilki.User;
-import com.infoshareacademy.jjdd6.wilki.Wallet;
+import com.infoshareacademy.jjdd6.wilki.*;
 import freemarker.template.Template;
 import freemarker.template.TemplateException;
 
 import javax.inject.Inject;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.transaction.Transactional;
 import java.io.IOException;
 import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
 
+@WebServlet("/details")
+@Transactional
 public class SpecifiedShareViewServlet extends HttpServlet {
 
     @Inject
@@ -57,9 +58,11 @@ public class SpecifiedShareViewServlet extends HttpServlet {
     }
 
     private void showSpecifiedShare(HttpServletRequest req, HttpServletResponse resp, String ticker) throws IOException {
+
         User user = userService.loggedUser(req);
         Wallet userWallet = user.getWallet();
         Share share = userWallet.scanWalletForShare(ticker);
+
         String tickerFromWallet = share.getTicker();
         Integer amount = share.getSharesTotalAmount();
         BigDecimal avgBuyPrice = share.getAvgBuyPrice();
@@ -70,20 +73,40 @@ public class SpecifiedShareViewServlet extends HttpServlet {
         BigDecimal stopLoss = share.getStopLossPrice();
         BigDecimal takeProfit = share.getTakeProfitPrice();
         String fullName = share.getFullCompanyName();
+        BigDecimal takeProfitValue = share.getTakeProfitValue();
+        BigDecimal stopLossValue = share.getStopLossValue();
+        Double currentPE = share.getCurrentPE();
+        Double targetPE = share.getTargetPE();
+        BigDecimal feeAmount = share.getFeeAmount();
+        Double riskReward = share.getRiskRewardRatio();
+        Long volume = share.getVolume();
+
+
         BigDecimal roe = userWallet.getROE();
         BigDecimal freeCash = userWallet.getFreeCash();
         String profilePicURL = userService.userProfilePicURL(user);
+
         DownloadCurrentData.updateWalletData(userWallet);
-        Map<String, Object> model = new HashMap<>();
+
         Map<String, String> bestPerforming = statsService.getMostProfitableShare(userWallet);
         Map<String, String> worstPerforming = statsService.getLeastProfitableShare(userWallet);
+
+        Map<String, Object> model = new HashMap<>();
+
+        model.put("takeProfitValue", takeProfitValue);
+        model.put("stopLossValue", stopLossValue);
+        model.put("currentPE", currentPE);
+        model.put("targetPE", targetPE);
+        model.put("feeAmount", feeAmount);
+        model.put("riskReward", riskReward);
+        model.put("volume", volume);
         model.put("ticker", tickerFromWallet);
         model.put("amount", amount);
         model.put("avgBuyPrice", avgBuyPrice);
         model.put("value", value);
         model.put("currentPrice", currentPrice);
         model.put("currentValue", currentValue);
-        model.put("return", currentReturn);
+        model.put("currentReturn", currentReturn);
         model.put("stopLoss", stopLoss);
         model.put("takeProfit", takeProfit);
         model.put("fullName", fullName);
