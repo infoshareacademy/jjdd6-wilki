a3e2502de50e8c75149fec402fddb6e6b6572a0d
Piotr Wenta <buraxta@gmail.com>
2019-05-21 10:04:47 +0200

    fix

--- web/src/main/java/com/infoshareacademy/jjdd6/servlet/StatsServlet.java
+++ web/src/main/java/com/infoshareacademy/jjdd6/servlet/StatsServlet.java
@@ -12,7 +12,6 @@ import freemarker.template.Template;
 import freemarker.template.TemplateException;
 
 import javax.inject.Inject;
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
